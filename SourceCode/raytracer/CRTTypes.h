4f45e29efec0acc8dbe24c72a62009cfe7a6d999
Danny Grigorov <dgrigoro@cisco.com>
2024-06-17 19:57:26 +0300

    hw5

--- SourceCode/raytracer/CRTTypes.h
+++ SourceCode/raytracer/CRTTypes.h
@@ -245,5 +245,75 @@ public:
         return "Triangle: {" + v0.toString() + ", " + v1.toString() + ", " + v2.toString() + "}";
     }
 
+    /*
+    * Input: ray, rayProj
+    * Input: rayProj: projection of ray.direction onto camera direction
+    * Output: t: distance from ray.origin to the intersection point
+    * Output: p: intersection point
+    * return: true if the ray intersects the triangle
+    */
+    bool intersect(const Ray& ray, float& t, Vec3& p) const {
+        // Here assuming counter-clockwise order
+        Vec3 e0 = v1 - v0;
+        Vec3 e1 = v2 - v0;
+        Vec3 tri_normal = cross(e0, e1).normalize();
 
+        float rayProj = dot(ray.direction, tri_normal);
+
+        // if triangle is facing the ray, compute ray-plane intersection
+        if (rayProj < -1e-6) {
+            float rpDist = dot(tri_normal, ray.origin - v0);
+            t = rpDist / rayProj;
+            p = ray.origin + ray.direction * t;
+
+            // check if `p` is inside triangle
+            Vec3 c0 = cross(e0, p - v0);
+            Vec3 c1 = cross(v2 - v1, p - v1);
+            Vec3 c2 = cross(v0 - v2, p - v2);
+
+            return dot(tri_normal, c0) > 1e-6 &&
+                   dot(tri_normal, c1) > 1e-6 &&
+                   dot(tri_normal, c2) > 1e-6;
+        }
+        return false;
+    }
+
+    bool intersect_plane(const Ray& ray, float& t, Vec3& p) const {
+        // Here assuming counter-clockwise order
+        Vec3 e0 = v1 - v0;
+        Vec3 e1 = v2 - v0;
+        Vec3 tri_normal = cross(e0, e1).normalize();
+
+        float rayProj = dot(ray.direction, tri_normal);
+
+        // if triangle is facing the ray, compute ray-plane intersection
+        if (rayProj < -1e-6) {
+            float rpDist = dot(tri_normal, v0 - ray.origin); // was this the problem?
+            t = rpDist / rayProj;
+            p = ray.origin + ray.direction * t;
+            return true;
+        }
+        return false;
+    }
+
+    bool intersect_plane2(const Ray& ray, float& t, Vec3& p) const
+    {
+        Vec3 e0 = v1 - v0;
+        Vec3 e1 = v2 - v0;
+        Vec3 n = cross(e0, e1).normalize();
+        Vec3 p0 = v0;
+        Vec3 l0 = ray.origin;
+        Vec3 l = ray.direction;
+
+        // Assuming vectors are all normalized
+        float denom = dot(n, l);
+        if (denom < -1e-6) {
+            Vec3 p0l0 = p0 - l0;
+            t = dot(p0l0, n) / denom; 
+            p = l0 + l * t;
+            return (t >= 0);
+        }
+
+        return false;
+    }
 };
